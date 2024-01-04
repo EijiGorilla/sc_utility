9194ba7c360d3eab1d8798e05f163e8f0efe24e8
EijiGorilla <emasuz@hotmail.com>
2024-01-04 13:38:25 +0800

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -162,7 +162,7 @@ function App() {
             style={{ marginBottom: 'auto', marginTop: 'auto' }}
           />
           <b className="headerTitle">SC UTILITY</b>
-          <div className="date">As of December 8, 2023</div>
+          <div className="date">As of December 29, 2023</div>
 
           <div className="dropdownFilter">
             <div className="dropdownFilterLayout">
